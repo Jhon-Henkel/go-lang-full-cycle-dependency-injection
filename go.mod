f79329c99a7c6835a9cc91ec169953eefbee6a17
Jhonatan Henkel <jhowrf@gmail.com>
2023-12-28 15:42:43 -0300

    build: criando aplicação base para o estudo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jhon-Henkel/go-lang-full-cycle-dependency-injection
+
+go 1.21.5
---
+++ product/usecase.go
@@ -0,0 +1,16 @@
+package product
+
+type ProductUseCase struct {
+	repository ProductRepository
+}
+
+func NewProductUseCase(repository ProductRepository) *ProductUseCase {
+	return &ProductUseCase{repository}
+}
+
+// GetProduct returns a Product by id.
+// This Product was not supported to be returned. We should a DTO instead.
+// However, we will return it for now to keep the example simple.
+func (uc *ProductUseCase) GetProduct(id int) (Product, error) {
+	return uc.repository.GetProduct(id)
+}
